69066709daf097c2b313643fb5db6e9851dfe0ae
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-10-26 15:06:08 -0500

    more work on basic layout

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,23 @@ var app = angular.module('wikipediaViewer', []);
 
 app.controller('wikiController', function($scope) {
     
-    $scope.searchAll = "Search Wikipedia";
+    $scope.searchAll = " Search Wikipedia";
+    var elm = document.querySelector('input');
     
     $scope.search = function () {
         console.log("search");
         $scope.searchAll = null;
-
-    }
+        elm.className="flex-item-grow-1 ";     
+    };
 
      $scope.random = function () {
         console.log("random");
         $scope.searchAll = null;
-    }
+        elm.className="flex-item-grow-1 ";
+    };
 
-
-
+    $scope.clearSuggestedText = function(){
+          $scope.searchAll = null;
+          elm.className="flex-item-grow-1 ";
+    }; 
 });
