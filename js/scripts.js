055b4f33b41cd32918e0340897947ec4e09341b6
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-10-26 17:03:19 -0500

    add $http service

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,46 @@
 var app = angular.module('wikipediaViewer', []);
 
-app.controller('wikiController', function($scope) {
+app.controller('wikiController', function($scope, $http) {
     
     $scope.searchAll = " Search Wikipedia";
     var elm = document.querySelector('input');
-    
+
     $scope.search = function () {
         console.log("search");
         $scope.searchAll = null;
-        elm.className="flex-item-grow-1 ";     
+        elm.className = "flex-item-grow-1 ";
+
+        $http({
+            method: 'POST',
+            url: 'http://en.wikipedia.org/w/api.php'
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            console.log(response);
+        });
+
+
+        function getSearchResults(data) {
+            console.log(data);
+        }
+
+        $("#submit").click(function () {
+            var searchTerm = $("input[name='search']").val();
+            var url = "http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + searchTerm + "&callback=?";
+            $.ajax({
+                type: "GET",
+                url: url,
+                async: true,
+                dataType: "jsonp",
+                success: function (data) {
+                    getSearchResults(data);
+                }
+            });
+});
+
     };
 
      $scope.random = function () {
@@ -22,3 +54,4 @@ app.controller('wikiController', function($scope) {
           elm.className="flex-item-grow-1 ";
     }; 
 });
+
